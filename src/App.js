bb38b871c9aa48c577676e541bd25868dcce37ea
Philip Clark <56705400+Philip-Clark@users.noreply.github.com>
2022-06-30 15:37:58 -0400

    Fix styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { GlobalStyles } from './components/styles/Global';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './components/styles/theme';
-import QuoteBox from './components/QuoteBox';
+// import { theme } from './components/styles/theme';
+import FloatingQuoteBox from './components/QuoteBox';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const theme = {
+    colors: {
+      card: '#D9D9D9',
+      body: '#BFBFBF',
+      pop: '#497a89',
+      text: '#4d4d4d',
+    },
+    fonts: {
+      imports:
+        "'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@1,300&family=Oswald:wght@300&display=swap'",
+      fontA: "'Oswald', sans-serif",
+      fontB: "'Cormorant Garamond', serif",
+    },
+  };
+
   const [dataLoaded, setDataLoaded] = useState(false);
   const [author, getAuthor] = useState('');
   const [text, getText] = useState('');
@@ -39,13 +54,13 @@ function App() {
       <>
         <GlobalStyles />
         <h1>Quotrr</h1>
-        <QuoteBox
+        <FloatingQuoteBox
           author={author}
           text={text}
           getQuote={getQuote}
           setDataLoaded={setDataLoaded}
           dataLoaded={dataLoaded}
-        ></QuoteBox>
+        ></FloatingQuoteBox>
       </>
       <footer>
         <a href="https://icons8.com/">Icons From Icons8</a>
